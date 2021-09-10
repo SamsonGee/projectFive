4f1ec4fa20e9057a7dcd88e2de51efc44c82287f
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-09-10 14:55:16 -0500

    gride set up

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//pseudo code this shit....
+
+// first it seems like the textarea is capturing any click and type... it's not saving 
+
+//since part of the activity is the save btn, i need to create a save function to local storage.
+
