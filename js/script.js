37fce2a81b7301b0ff83daf1cf4b96a1eaed94c2
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-09-12 23:45:32 -0500

    finished functionality

--- js/script.js
+++ js/script.js
@@ -1,51 +1,29 @@
 var currentTime = moment().format('MMM Do h:mm A');
 $("#currentDay").text(currentTime);
 
-var currentHour = moment().format('h');
+var currentHour = moment().format('H');
 
 var past
 var current
 var future
 var taskHour
-/* console.log(moment().format('h'));
-console.log($('.hour col-lg-2 col-sm-12'));
-$(".row").each(function(){
-    if(moment().format("h") === $(this).val()) {
-        taskHour = current;
-        console.log(taskHour)
-    }
-console.log($(this).val())
-}) */
 
 
-//pseudo code this shit....
-
-var tasks = {};
-
-/* var createTask = function(taskText, taskTime) {
-    // create elements that make up a task item
-    var task
-} */
-
-//literally copying code from taskmaster-pro]
-
-  //so this code is setting a string of the object of "tasks" a variable i have yet to define, which is great, because I can build around this.
-
-  // while i'm at it, i'm also going to copy the load tasks function
-
-    // loop over object properties
-
-  // copying the push button logic in taskmaster
+// when save button is clicked
 
   $(".saveBtn").on("click", function(){
+
+    //create a variable for text and the time 
       var textValue = $(this).siblings(".text").val();
       var textTime = $(this).siblings(".text").attr("hour");
-      console.log(textTime);
-      console.log(textValue);
+      /* console.log(textTime);
+      console.log(textValue); */
 
+      //save in local storage 
       localStorage.setItem(textTime, textValue);
   })
 
+  //call back any prevoiusly saved tasks using an array 
   function getTasks() {
     var arr = [9, 10, 11, 12, 13, 14,15, 16, 17];
     arr.forEach(function(e){
@@ -53,11 +31,21 @@ var tasks = {};
     })
   }
 
-/* function timeOfDay() {
+function timeOfDay() {
     $("textarea").each(function(){
-        var timeBlock = parseInt($(this).attr("hour"));
-        console.log(timeBlock);
+        var timeBlock = parseInt($(this).attr("hour"));  
+        /* console.log(moment().format('h')); */
+        if(timeBlock < currentHour){
+            $(this).addClass("past")
+        }
+        else if(timeBlock === currentHour){
+            $(this).addClass("present")
+        }
+        else if(timeBlock > currentHour) {
+            $(this).addClass("future")
+        }
     })
-} */
+};
 
 getTasks();
+timeOfDay();
