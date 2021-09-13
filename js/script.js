c1697d1cf5e12cf361f13181ee20a3bbd016312a
SamsonGee <88246655+SamsonGee@users.noreply.github.com>
2021-09-12 23:26:38 -0500

    save text options done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 var currentTime = moment().format('MMM Do h:mm A');
 $("#currentDay").text(currentTime);
 
+var currentHour = moment().format('h');
+
 var past
 var current
 var future
 var taskHour
-console.log(moment().format('h'));
+/* console.log(moment().format('h'));
 console.log($('.hour col-lg-2 col-sm-12'));
 $(".row").each(function(){
     if(moment().format("h") === $(this).val()) {
@@ -13,17 +15,10 @@ $(".row").each(function(){
         console.log(taskHour)
     }
 console.log($(this).val())
-})
+}) */
 
 
 //pseudo code this shit....
-var textInput = document.querySelector(".task").textContent;
-// first it seems like the textarea is capturing any click and type... it's not saving 
-
-//since part of the activity is the save btn, i need to create a save function to local storage.
-
-// let's do that now, i know we can do that from a previous code.
-//let's first create a variable to capture the task
 
 var tasks = {};
 
@@ -34,37 +29,35 @@ var tasks = {};
 
 //literally copying code from taskmaster-pro]
 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
-
   //so this code is setting a string of the object of "tasks" a variable i have yet to define, which is great, because I can build around this.
 
   // while i'm at it, i'm also going to copy the load tasks function
-  var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  
-    // if nothing in localStorage, create a new object to track all task status arrays
-    if (!tasks) {
-      tasks = {
-      };
-    }
-  
+
     // loop over object properties
-    $.each(tasks, function(list, arr) {
-      // then loop over sub-array
-      arr.forEach(function(task) {
-        createTask(task.text);
-      });
-    });
-  };
 
   // copying the push button logic in taskmaster
 
-  $(".task").on("click", "textarea", function () {
-      //get current status of text of textarea
-      var text = $(textInput).text;
-      console.log(text);
+  $(".saveBtn").on("click", function(){
+      var textValue = $(this).siblings(".text").val();
+      var textTime = $(this).siblings(".text").attr("hour");
+      console.log(textTime);
+      console.log(textValue);
+
+      localStorage.setItem(textTime, textValue);
   })
 
-  var time = document.querySelector("#currentDay").textContent;
+  function getTasks() {
+    var arr = [9, 10, 11, 12, 13, 14,15, 16, 17];
+    arr.forEach(function(e){
+        $("textarea[hour='" + e + "']").val(localStorage.getItem(e))
+    })
+  }
+
+/* function timeOfDay() {
+    $("textarea").each(function(){
+        var timeBlock = parseInt($(this).attr("hour"));
+        console.log(timeBlock);
+    })
+} */
+
+getTasks();
